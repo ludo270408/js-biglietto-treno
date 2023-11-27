59b0e19f08fc3cf16862c7b22d5ac7444babe855
ludo270408 <ludovica.sette7@gmaio.com>
2023-11-27 03:12:02 +0100

    [UPTADE] If done

--- script.js
+++ script.js
@@ -7,5 +7,9 @@ let prezzo_biglietto=0.21*chilometri
 let prezzo_totale;
 
 if(eta<18){
-    prezzo_totale=
+    prezzo_totale=(prezzo_biglietto*0.8);
+} else(eta>65){
+    prezzo_totale=(prezzo_biglietto*0.6);
 }
+
+console.log(prezzo_totale);
