ea41424d185e6510e7dbad198b531edac6e8c1e3
ludo270408 <ludovica.sette7@gmaio.com>
2023-11-27 22:54:51 +0100

    [UPTADE] Prfoblem solved

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
-let chilometri=prompt('Inserisci i Km da percorrere: ');
+let chilometri= prompt('Inserisci i Km da percorrere: ');
 
 let eta=prompt('Inserisci la tua età: ');
 
-let prezzo_biglietto=0.21*chilometri
+let prezzo_biglietto=0.21*chilometri; 
 
 let prezzo_totale;
 
 if(eta<18){
     prezzo_totale=(prezzo_biglietto*0.8);
-} else(eta>65){
+} else(eta>65)
+{
     prezzo_totale=(prezzo_biglietto*0.6);
 }
 
