de7584691fc7238c28185a8f046a5a109fd3b203
ludo270408 <ludovica.sette7@gmaio.com>
2023-11-27 03:09:34 +0100

    [UPTADE] Variables added

--- script.js
+++ script.js
@@ -2,4 +2,10 @@ let chilometri=prompt('Inserisci i Km da percorrere: ');
 
 let eta=prompt('Inserisci la tua età: ');
 
+let prezzo_biglietto=0.21*chilometri
+
 let prezzo_totale;
+
+if(eta<18){
+    prezzo_totale=
+}
